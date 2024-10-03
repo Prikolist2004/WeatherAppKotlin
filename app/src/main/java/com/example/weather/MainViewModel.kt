ed450991503a10bff3fca3e0ee40251413aebb62
Prikolist2004 <Aytist39@gmail.com>
2024-10-03 18:23:44 +0900

    Second commit

--- app/src/main/java/com/example/weather/MainViewModel.kt
+++ app/src/main/java/com/example/weather/MainViewModel.kt
@@ -2,9 +2,10 @@ package com.example.weather
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weather.Adapters.WeatherModel
 
 class MainViewModel:ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 
 }
