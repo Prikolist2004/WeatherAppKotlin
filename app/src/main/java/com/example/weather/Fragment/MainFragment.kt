b74f2a2de5b3c81080c18b780027e54e94fdd825
Prikolist2004 <Aytist39@gmail.com>
2024-10-05 13:20:22 +0900

    commit №4

--- app/src/main/java/com/example/weather/Fragment/MainFragment.kt
+++ app/src/main/java/com/example/weather/Fragment/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.weather.Fragment
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +11,14 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weather.Adapters.VpAdapter
 import com.example.weather.R
 import com.example.weather.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
-
+const val API_KEY = "a17e5aac8720464382955808242109"
 
 class MainFragment : Fragment() {
     private val fList = listOf(
@@ -42,6 +46,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         chekPermission()
         init()
+        requestWeatherData("Nigger")
 
     }
     private fun init() = with(binding){
@@ -64,6 +69,29 @@ class MainFragment : Fragment() {
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
+    private fun requestWeatherData(city: String)
+    {
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=" +
+                city +
+                "&days=" +
+                "3" +
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                result ->Log.d("MyLog", "Result: $result")
+            },
+            {
+                error -> Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(request)
+    }
+
 
     companion object {
         @JvmStatic
